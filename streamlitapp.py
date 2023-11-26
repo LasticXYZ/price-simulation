ffdfe65f265b29c6d7185dec1664eded732e0a56
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-11-26 17:21:41 +0100

    separating functionality

--- streamlitapp.py
+++ streamlitapp.py
@@ -42,54 +42,74 @@ class StreamlitApp:
             "limit_cores_offered": "Artificial limit to the number of cores allowed to be sold.",
             "renewal_bump": "Amount by which the renewal price increases each sale period."
         }
-
-    def get_slider_input(self):
-        # Create a slider for the sale start and price
-
-        observe_time = st.slider('X-AXIS - Observing time', min_value= 1, max_value=20, value=2, step=1, help='Number of regions to observe: Nb.of regions (28 day chunks)')
+    
+    def get_observation_time_input(self):
+        """
+        Create a slider to set the observation time.
+        """
+        observe_time = st.slider(
+            'X-AXIS - Observing time', min_value=1, max_value=20, value=2, step=1,
+            help='Number of regions to observe: Nb.of regions (28 day chunks)'
+        )
         observe_blocks = observe_time * self.config.region_length
+        return observe_blocks
 
-        initial_bought_price = st.slider('Y-AXIS - Start Price of the Core You Bought', min_value=0, max_value=10000, value=1000, step=10)
+    def get_price_input(self):
+        """
+        Create sliders for setting the initial bought price and starting price.
+        """
+        initial_bought_price = st.slider(
+            'Y-AXIS - Start Price of the Core You Bought', min_value=0, max_value=10000, value=1000, step=10
+        )
         self.price_calculator.change_bought_price(initial_bought_price)
-        price = st.slider('Y-AXIS Starting Price', min_value=0, max_value=10000, value=1000, step=10)
+
+        price = st.slider(
+            'Y-AXIS Starting Price', min_value=0, max_value=10000, value=1000, step=10
+        )
         self.price_calculator.change_initial_price(price)
 
+    def get_factor_curve_input(self):
+        """
+        Create inputs for setting the factor curve.
+        """
         with st.expander("Factor curve"):
             st.write("Change the lead-in factor (LF) curve - To exponential or linear")
-
             linear = st.toggle('-', value=True, help='Toggle between exponential and linear')
-            if linear:
-                linear_text = 'Current value: Linear'
-            else:
-                linear_text = 'Current value: Exponential'
+            linear_text = 'Current value: Linear' if linear else 'Current value: Exponential'
             st.write(linear_text)
 
             self.price_calculator.change_linear(linear)
-            factor_value = st.slider('Factor Value', min_value=1, max_value=10, value=1, step=1, help='Factor value for the lead-in factor curve')
+            factor_value = st.slider(
+                'Factor Value', min_value=1, max_value=10, value=1, step=1,
+                help='Factor value for the lead-in factor curve'
+            )
             self.price_calculator.change_factor(factor_value)
-        
             st.write(self.price_calculator.get_factor(), self.price_calculator.get_linear())
 
+    def get_cores_input(self):
+        """
+        Create sliders for setting the number of cores renewed and sold in each sale.
+        """
         st.header("Cores Renewed and Sold in Each Sale")
-        renewed_cores_in_each_sale = st.slider('Cores renewed in each sale', min_value=0, max_value=self.config.limit_cores_offered, value=10, step=1)
-        if self.config.limit_cores_offered - renewed_cores_in_each_sale > 0:
-            sold_cores_in_each_sale = st.slider('Cores sold in each sale', min_value=0, max_value=self.config.limit_cores_offered - renewed_cores_in_each_sale, value=0, step=1)
-        else:
-            sold_cores_in_each_sale = 0
+        renewed_cores_in_each_sale = st.slider(
+            'Cores renewed in each sale', min_value=0, max_value=self.config.limit_cores_offered, value=10, step=1
+        )
 
-        st.markdown("### Adjustment for each region length (28 days)")
-        monthly_renewals = {}
-        monthly_sales = {}
-        for month in range(1, observe_time + 1):
-            with st.expander(f"Region {month} Adjustments"):
-                renewed_cores = st.slider(f'Cores renewed in Month {month}', min_value=0, max_value=self.config.limit_cores_offered, value=10, step=1)
-                if self.config.limit_cores_offered - renewed_cores > 0:
-                    sold_cores = st.slider(f'Cores sold in Month {month}', min_value=0, max_value=self.config.limit_cores_offered - renewed_cores, value=0, step=1)
-                else:
-                    sold_cores = 0
-                monthly_renewals[month] = renewed_cores
-                monthly_sales[month] = sold_cores
-            st.write("Region nb. ", month, ": Renewals ", renewed_cores, ", Sold ", sold_cores)
+        max_sold_cores = self.config.limit_cores_offered - renewed_cores_in_each_sale
+        sold_cores_in_each_sale = 0 if max_sold_cores <= 0 else st.slider(
+            'Cores sold in each sale', min_value=0, max_value=max_sold_cores, value=0, step=1
+        )
+
+        return renewed_cores_in_each_sale, sold_cores_in_each_sale
+
+    def get_slider_input(self):
+        """
+        Combine all slider inputs into one method.
+        """
+        observe_blocks = self.get_observation_time_input()
+        self.get_price_input()
+        self.get_factor_curve_input()
+        renewed_cores_in_each_sale, sold_cores_in_each_sale = self.get_cores_input()
 
         return observe_blocks, renewed_cores_in_each_sale, sold_cores_in_each_sale
 
@@ -100,12 +120,7 @@ class StreamlitApp:
         ax.axvline(x=region_start + self.config.interlude_length + self.config.leadin_length, color='g', linestyle='--')
         ax.axvline(x=region_start + self.config.region_length, color='b', linestyle='--')
 
-    def run(self):
-        """
-        Run the Streamlit application.
-        """
-        st.title('Sale Price over Time')
-        
+    def _create_sidebar(self):
         # Sidebar for Configuration Input
         with st.sidebar:
             st.header("Configuration Settings")
@@ -117,9 +132,14 @@ class StreamlitApp:
             st.header("Sale Settings")
             observe_blocks, renewed_cores_in_each_sale, sold_cores_in_each_sale = self.get_slider_input()
 
+        return observe_blocks, renewed_cores_in_each_sale, sold_cores_in_each_sale
+
+    def _explaination_section(self):
         st.markdown(create_tooltip("Red-Yellow: INTERLUDE PERIOD", "The area between the red and yellow section represents the INTERLUDE Period, this is the time when accounts who bought their cores in previous blocks can renew them."), unsafe_allow_html=True)
         st.markdown(create_tooltip("Yellow-Green: LEADIN PERIOD", "The area between the yellow and green section represents the LEADIN Period, this is the time when new sales occur."), unsafe_allow_html=True)
+        st.markdown(create_tooltip("Red-Red: REGION PERIOD", "The area between two red sections represents the REGION Period, this represents one region length."), unsafe_allow_html=True)
 
+    def _plot_graph(self, observe_blocks, renewed_cores_in_each_sale, sold_cores_in_each_sale):
         region_nb = int(observe_blocks / self.config.region_length)
 
         fig, ax = plt.subplots()
@@ -140,3 +160,14 @@ class StreamlitApp:
         ax.set_title('Sale Price over Time')
         ax.legend()
         st.pyplot(fig)
+
+    def run(self):
+        """
+        Run the Streamlit application.
+        """
+        observe_blocks, renewed_cores_in_each_sale, sold_cores_in_each_sale = self._create_sidebar()
+
+        st.title('Sale Price over Time')
+
+        self._explaination_section()
+        self._plot_graph(observe_blocks, renewed_cores_in_each_sale, sold_cores_in_each_sale)
